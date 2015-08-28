e41a11455eb566276a9216755bf80f7fad3690e8
jaffar <jaffar.almaleki@gmail.com>
2015-08-28 22:17:21 +0100

    Changes
    further exploration of the features.

--- bot.py
+++ bot.py
@@ -1,20 +1,52 @@
 import Skype4Py
+import codecs
+import hashlib
+import logging
 
 # Create an instance of the Skype class.
 skype = Skype4Py.Skype()
 
-# Variable to check if the script has attached to skype sucessfully (skype.attach())
+# Variable to check if the script has attached to skype Successfully (skype.attach())
 status = Skype4Py.apiAttachSuccess
 
 # Checks if the Skype Object is already connected to the Skype Client Sucessfully then
-# Connects the Skype object to the Skype client if its not Sucessfull, otherwise
+# Connects the Skype object to the Skype client if its not Successfull, otherwise
 # Does nothing.
-if status == 0:
-    pass
-    print 'passed'
-else:
+if status == 1:
     skype.Attach()
     print 'attached'
+else:
+    pass
+    print 'passed'
+
+print skype.Conferences[0]
+
+# Pulls conversations and outputs them to a file.
+#LogFile = 'TeamUR.log'
+#iteratedObject = skype.Messages(Target =
+#for x in skype.ActiveChats:
+#    if len(x.Members) > 3:)
+
+def get_chat_id(chat):
+    """
+    Get unique internal persistent id of the chat object.
+
+    All ids are URL safe.
+
+    This is the same id as in the web interface.
+
+    :param chat: Skype4Py.chat.Chat instance
+    """
+    m = hashlib.md5()
+    m.update(chat.Name)
+    return m.hexdigest()
+
+def FindGroupTarget():
+    for x in skype.ActiveChats:
+        if len(x.Members) > 20:
+            return x
+        else:
+            pass
 
 # Asks for a Username and Message, then sends the message.
 while 0==0:
