cb3574de5f0566d1b882278c5247621661acfc9e
jaffar <jaffar.almaleki@gmail.com>
2015-08-28 19:57:32 +0100

    Added features and got the File working.
    Added a check to see if the script is already attached to the Skype
    client then attaches if it isn't, does nothing if it is. also added a
    way to send messages manually (use as an example of how to use the
    command)

--- bot.py
+++ bot.py
@@ -3,5 +3,21 @@ import Skype4Py
 # Create an instance of the Skype class.
 skype = Skype4Py.Skype()
 
-# Connect the Skype object to the Skype client.
-skype.Attach() 
+# Variable to check if the script has attached to skype sucessfully (skype.attach())
+status = Skype4Py.apiAttachSuccess
+
+# Checks if the Skype Object is already connected to the Skype Client Sucessfully then
+# Connects the Skype object to the Skype client if its not Sucessfull, otherwise
+# Does nothing.
+if status == 0:
+    pass
+    print 'passed'
+else:
+    skype.Attach()
+    print 'attached'
+
+# Asks for a Username and Message, then sends the message.
+while 0==0:
+    Usr = raw_input('Who would you like to Send a message to (Skype Username): ')
+    Msg = raw_input('What would you like to Send: ')
+    skype.SendMessage(Usr, Msg)
